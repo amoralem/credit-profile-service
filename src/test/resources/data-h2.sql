95725fa9c0c98740cb30e75b0630445a18b5fdcb
Angel <out_amoralesm@independencia.com.mx>
2021-10-17 16:55:03 -0500

    add: se agregan datos en el script data.sql

--- src/test/resources/data-h2.sql
+++ src/test/resources/data-h2.sql
@@ -2,10 +2,97 @@ delete from profiles;
 
 INSERT INTO profiles
 (id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
-VALUES(1, 'Shopping', 7000, 14999, 18, 23, 'B*smart, Afinity Card');
+VALUES(1, 'Shopping', 7000.0000, 14999.0000, 18, 23, 'B*smart, Afinity Card');
 INSERT INTO profiles
 (id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
-VALUES(2, 'Shopping', 7000, 14999, 24, 32, 'B*smart, Clasica, Afinity Card, Office Depot, Costco, Best Buy Banamex, The Home Depot');
+VALUES(2, 'Shopping', 7000.0000, 14999.0000, 24, 32, 'B*smart, Clasica, Afinity Card, Office Depot, Costco, Best Buy Banamex, The Home Depot');
 INSERT INTO profiles
 (id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
-VALUES(3, 'Shopping', 7000, 14999, 33, 75, 'Clasica, Office Depot, Costco, Best Buy Banamex, The Home Depot');
+VALUES(3, 'Shopping', 7000.0000, 14999.0000, 33, 75, 'Clasica, Office Depot, Costco, Best Buy Banamex, The Home Depot');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(4, 'Shopping', 15000.0000, 34999.0000, 18, 23, 'B*smart, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(5, 'Shopping', 15000.0000, 34999.0000, 24, 32, 'Oro, Afinity Card, Office Depot, Costco, Best Buy Banamex, The Home Depot');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(6, 'Shopping', 15000.0000, 34999.0000, 33, 75, 'Oro, Office Depot, Costco, Best Buy Banamex, The Home Depot');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(7, 'Shopping', 35000.0000, 49999.0000, 18, 23, 'B*smart, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(8, 'Shopping', 35000.0000, 49999.0000, 24, 75, 'Platinum, Office Depot, Costco, Best Buy Banamex, The Home Depot');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(10, 'Shopping', 50000.0000, 0.0000, 24, 75, 'Platinum');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(9, 'Shopping', 50000.0000, 0.0000, 18, 23, 'B*smart, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(11, 'Travels', 12000.0000, 29999.0000, 18, 75, 'Citi / AAdvantage');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(12, 'Travels', 30000.0000, 34999.0000, 18, 23, 'Citi / AAdvantage');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(13, 'Travels', 30000.0000, 34999.0000, 24, 75, 'Citi / AAdvantage, Platinum');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(14, 'Travels', 35000.0000, 49999.0000, 18, 23, 'Citi / AAdvantage');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(15, 'Travels', 35000.0000, 49999.0000, 24, 75, 'Citi / AAdvantage, Platinum');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(16, 'Travels', 50000.0000, 0.0000, 18, 22, 'Citi / AAdvantage');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(17, 'Travels', 50000.0000, 0.0000, 23, 75, 'Platinum');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(18, 'Help', 12000.0000, 0.0000, 18, 75, 'Banamex Teletón, APAC');
+INSERT INTO profiles
+(your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES('My business', 7000, 0, 18, 75, 'Office Depot, Costco, Best Buy Banamex, The Home Depot');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(20, 'Sports', 7000.0000, 34999.0000, 18, 75, 'Marti Clasica Citibanamex, América Deporteísmo, Pumas Deporteísmo, Toluca Deporteísmo, La verde Deporteísmo');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(21, 'Sports', 35000.0000, 0.0000, 18, 75, 'Marti Premium Citibanamex');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(22, 'My style', 7000.0000, 14999.0000, 18, 32, 'B*smart, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(23, 'My style', 7000.0000, 14999.0000, 33, 75, 'B*smart');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(24, 'My style', 15000.0000, 29999.0000, 18, 23, 'B*smart, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(25, 'My style', 15000.0000, 29999.0000, 24, 32, 'B*smart, Oro, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(26, 'My style', 15000.0000, 29999.0000, 33, 75, 'Oro');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(27, 'My style', 30000.0000, 34999.0000, 18, 23, 'B*smart, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(28, 'My style', 30000.0000, 34999.0000, 24, 32, 'B*smart, Platinum, Afinity Card');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(29, 'My style', 30000.0000, 34999.0000, 33, 75, 'Platinum');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(30, 'My style', 35000.0000, 0.0000, 18, 23, 'B*smart');
+INSERT INTO profiles
+(id, your_passion, monthly_salary_min, monthly_salary_max, age_min, age_max, credit_cards)
+VALUES(31, 'My style', 35000.0000, 0.0000, 24, 75, 'B*smart, Platinum');
+
+
+
